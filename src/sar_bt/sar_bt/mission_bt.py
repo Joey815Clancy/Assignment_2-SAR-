a6a0b32804cfa17006c6b30d667efcb7093c6c6b
Joey815Clancy <23375817@studentmail.ul.ie>
2026-05-09 16:16:07 +0100

    add sar_bt package(unfinsihed/ not working and update bridge to include /odom

---
+++ src/sar_bt/sar_bt/mission_bt.py
@@ -0,0 +1,43 @@
+import rclpy
+from rclpy.node import Node
+
+import py_trees
+import py_trees_ros
+
+# Your behaviour nodes
+from sar_bt.behaviours.navigate_to import NavigateTo
+from sar_bt.behaviours.fire_safety import FireSafety
+from sar_bt.behaviours.battery_check import BatteryCheck
+from sar_bt.behaviours.scan_dam import ScanDam
+from sar_bt.behaviours.publish_tf import PublishTF
+
+class MissionBT Node():
+    def __init__(self):
+
+        # Build tree
+
+        root = py_tree.composites.Sequence(name="Mission",memory=True)
+
+        fire_safety= FireSafety()
+        task1 = build_task1_subtree()
+        task2 = build_task2_subtree()
+        task5 = build_task5_subtree()
+
+        root.add_children([fire_safety, task1, task2, task5])
+
+        # Tick every 100ms
+
+        self.timer = self.create_timer(0.1,self.tick)
+
+    def tick(self.tick)
+        self.tree.tick()
+def main(args=None):
+    rclpy.init(args=args)
+    try:
+        node = MissionBT()
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == '__main__':
+    main()
---
+++ src/sar_bt/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'sar_bt'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='joeyc',
+    maintainer_email='23375817@studentmail.ul.ie',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'mission_bt = sar_bt.mission_bt:main',
+        ],
+    },
+)
